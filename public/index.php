8f84e518d352b60cda69d07aa963d55259103705
TakahiroTsuchiya <takahirotsuchiya@my-notebook.local>
2016-09-19 21:54:57 +0900

    test Post Request.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "Hello World.";
+// echo "Hello World.";
+
+if isset($_POST["comment"]) {
+  $comment = $_POST[‘comment’];
+  echo $comment;
+}
+?>
+<!DOCTYPE html>
+<html lang = “ja”>
+<head>
+  <meta charset = “UFT-8”>
+  <title>Sample</title>
+</head>
+<body>
+  <h1>Post Request</h1>
+  <form action = “index.php” method = “post”>
+    <input type = “text” name =“comment/“><br/>
+    <input type = “submit” value =“send“>
+  </form>
+</body>
+</html>
